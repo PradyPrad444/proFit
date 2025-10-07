97b0d9f9fafba651aa1114f6108ce0abf5bea323
PradyZaddy <138837542+PradyZaddy@users.noreply.github.com>
2025-10-07 00:44:30 -0600

    BackgroundRemoval Pipeline
    1. used FastAPI to make the upload endpoint that takes a file as a path parameter and sends to the rembg API and receives the processed image.
    2. created database for the project storing all the essential information about the items
    3. once processed, the details of the item is sent to the database

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+import certifi
+ca = certifi.where()
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import ssl
+from pymongo import MongoClient
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://prady:Prady444@profit.s9amval.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(
+    uri,
+    server_api=ServerApi('1'),
+    tls=True,                              # enable TLS/SSL
+    tlsAllowInvalidCertificates=True       # skip certificate verification (temporary)
+)
---
+++ backend/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from pymongo import MongoClient
+from config import client #used from config.py
+from rembg import remove
+import os, uuid
+import certifi
+ca = certifi.where()
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "This is the home page"
+
+# choosing the client -> creating the database -> creating the collection
+db = client["profit"]
+collection = db["wardrobe"]
+
+# create a new folder/directory called media if it does not exits
+MEDIA_DIR = "media"
+os.makedirs(MEDIA_DIR, exist_ok=True)
+
+# mounting the fastAPI with this media folder so that this folder can be an end point
+app.mount("/media", StaticFiles(directory=MEDIA_DIR), name="media")
+
+@app.post('/upload')
+async def uploadImage(file: UploadFile = File(...)):
+    contents = await file.read()
+    output = remove(contents)
+    item_id = str (uuid.uuid4())
+    output_path = os.path.join(MEDIA_DIR, f"{item_id}.png")
+
+    with open(output_path, 'wb') as f:
+        f.write(output)
+
+
+    doc = {
+        "item_id" : item_id,
+        "file_path" : output_path,
+        "url" : f"http://127.0.0.1:8000/media/{item_id}.png"
+    }
+
+    collection.insert_one(doc)
+
+    return JSONResponse({"id": item_id, "image_url": doc["url"]})
+
+
